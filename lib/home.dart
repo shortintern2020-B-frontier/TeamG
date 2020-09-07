a09f6b4cd038d8d0a9fd97489cd2212653a08b71
Hiroki Fujioka <hiroki976@gmail.com>
2020-09-07 14:58:15 +0900

    #11 change aboutMe -> latest message

--- lib/home.dart
+++ lib/home.dart
@@ -44,8 +44,9 @@ class HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    registerNotification();
-    configLocalNotification();
+    // 通知機能を実装する際に必要となるので残しておく。
+    // registerNotification();
+    // configLocalNotification();
     _pageController = new PageController();
   }
 
@@ -55,43 +56,62 @@ class HomeScreenState extends State<HomeScreen> {
     _pageController.dispose();
   }
 
-  void registerNotification() {
-    firebaseMessaging.requestNotificationPermissions();
+  // void registerNotification() {
+  //   firebaseMessaging.requestNotificationPermissions();
 
-    firebaseMessaging.configure(onMessage: (Map<String, dynamic> message) {
-      print('onMessage: $message');
-      Platform.isAndroid
-          ? showNotification(message['notification'])
-          : showNotification(message['aps']['alert']);
-      return;
-    }, onResume: (Map<String, dynamic> message) {
-      print('onResume: $message');
-      return;
-    }, onLaunch: (Map<String, dynamic> message) {
-      print('onLaunch: $message');
-      return;
-    });
+  //   firebaseMessaging.configure(onMessage: (Map<String, dynamic> message) {
+  //     print('onMessage: $message');
+  //     Platform.isAndroid
+  //         ? showNotification(message['notification'])
+  //         : showNotification(message['aps']['alert']);
+  //     return;
+  //   }, onResume: (Map<String, dynamic> message) {
+  //     print('onResume: $message');
+  //     return;
+  //   }, onLaunch: (Map<String, dynamic> message) {
+  //     print('onLaunch: $message');
+  //     return;
+  //   });
 
-    firebaseMessaging.getToken().then((token) {
-      print('token: $token');
-      FirebaseFirestore.instance
-          .collection('users')
-          .doc(currentUserId)
-          .update({'pushToken': token});
-    }).catchError((err) {
-      Fluttertoast.showToast(
-          msg: err.message.toString(), backgroundColor: Colors.red);
-    });
-  }
+  //   firebaseMessaging.getToken().then((token) {
+  //     print('token: $token');
+  //     FirebaseFirestore.instance
+  //         .collection('users')
+  //         .doc(currentUserId)
+  //         .update({'pushToken': token});
+  //   }).catchError((err) {
+  //     Fluttertoast.showToast(
+  //         msg: err.message.toString(), backgroundColor: Colors.red);
+  //   });
+  // }
 
-  void configLocalNotification() {
-    var initializationSettingsAndroid =
-        new AndroidInitializationSettings('app_icon');
-    var initializationSettingsIOS = new IOSInitializationSettings();
-    var initializationSettings = new InitializationSettings(
-        initializationSettingsAndroid, initializationSettingsIOS);
-    flutterLocalNotificationsPlugin.initialize(initializationSettings);
-  }
+  // void configLocalNotification() {
+  //   var initializationSettingsAndroid =
+  //       new AndroidInitializationSettings('app_icon');
+  //   var initializationSettingsIOS = new IOSInitializationSettings();
+  //   var initializationSettings = new InitializationSettings(
+  //       initializationSettingsAndroid, initializationSettingsIOS);
+  //   flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  // }
+
+  // void showNotification(message) async {
+  //   var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+  //     'com.hikomaryu.echo',
+  //     'Flutter chat demo',
+  //     'your channel description',
+  //     playSound: true,
+  //     enableVibration: true,
+  //     importance: Importance.Max,
+  //     priority: Priority.High,
+  //   );
+  //   var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+  //   var platformChannelSpecifics = new NotificationDetails(
+  //       androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+
+  //   await flutterLocalNotificationsPlugin.show(0, message['title'].toString(),
+  //       message['body'].toString(), platformChannelSpecifics,
+  //       payload: json.encode(message));
+  // }
 
   void onItemMenuPress(Choice choice) {
     if (choice.title == 'Log out') {
@@ -100,33 +120,6 @@ class HomeScreenState extends State<HomeScreen> {
       Navigator.push(
           context, MaterialPageRoute(builder: (context) => ChatSettings()));
     }
-  }
-
-  void showNotification(message) async {
-    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
-      'com.hikomaryu.echo',
-      'Flutter chat demo',
-      'your channel description',
-      playSound: true,
-      enableVibration: true,
-      importance: Importance.Max,
-      priority: Priority.High,
-    );
-    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
-    var platformChannelSpecifics = new NotificationDetails(
-        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-
-    print(message);
-//    print(message['body'].toString());
-//    print(json.encode(message));
-
-    await flutterLocalNotificationsPlugin.show(0, message['title'].toString(),
-        message['body'].toString(), platformChannelSpecifics,
-        payload: json.encode(message));
-
-//    await flutterLocalNotificationsPlugin.show(
-//        0, 'plain title', 'plain body', platformChannelSpecifics,
-//        payload: 'item x');
   }
 
   Future<Null> handleSignOut() async {
